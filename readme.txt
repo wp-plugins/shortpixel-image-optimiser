fd46732ab7e1d743477a313473e0aa1ed374a1d9
ShortPixel <ShortPixel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-05 09:37:53 +0000

    ShortPixel Image Optimiser 1.0
    git-svn-id: https://plugins.svn.wordpress.org/shortpixel-image-optimiser/tags/1.0@1020099 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,72 @@
+ === ShortPixel ===
+
+Contributors: AlexSP
+Tags: image optimisation, compression, lossy, lossless, performance, upload speed, fast, image quality
+Requires at least: 3.0.0 or higher
+Tested up to: 4.0
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+The ShortPixel WordPress plugin optimizes images automaticaly using both lossy and lossless compression, thus improving your website performance.
+
+== Description ==
+
+The ShortPixel WordPress plugin optimizes images automaticaly using both lossy and lossless compression, thus improving your website performance.
+Resulting, smaller, images are no different in quality from the original. 
+
+
+ShortPixel uses advanced compression technology that reduces image size without any before/after differences. 
+
+Features included:
+   - Both lossy and lossless optimisation 
+   - Supported formats: JPG, PNG, GIF
+   - Batch image optimisation
+   - Powerful algorithms
+   - Quick backups
+
+   - Less bandwidth usage
+   - Up to 90% compression rate
+   - Better search ranking
+   - Super fast websites
+
+On the ShortPixel.com website, we are offering an API you can also use for image optimisation purposses.
+
+
+== Installation ==
+
+Let's get ShortPixel plugin running on your WordPress website:
+
+
+1. Sign up using your email at http://shortpixel.com/free-sign-up
+2. You will receive your personal API key in a confirmation email, to the address you provided.
+3. Upload the ShortPixel plugin to the /wp-content/plugins/ directory
+4. Use your unique API key to activate ShortPixel plugin in the 'Plugins' menu in WordPress.
+5. Uploaded images can be automatically optimised in the Media Library.
+6. Done!
+
+
+== Frequently Asked Questions ==
+
+= Should I pick lossy or losssless optimisation =
+
+This depends on your compression needs. Lossy has a better compression rate than lossless compression. 
+The resulting image is not 100% identical with the original. Works well for photos taken with your camera.
+
+With lossless compression, the shrunk image will be identical with the original and smaller in size. 
+Use this when you do not want to loose any of the original image's details. Works best for technical drawings, clip art and comics.
+
+
+= Is my privacy ensured, are all my images safe? =
+
+Yes, privacy is guaranteed. The ShortPixel automated encription process doesn't allow anyone to view your photos.
+
+== Screenshots ==
+
+1. ShortPixel_options_screenshot
+2. ShortPixel_plugin_screenshot
+
+== Changelog ==
+
+
+
---
+++ shortpixel_api.php
@@ -0,0 +1,162 @@
+<?php
+if ( !function_exists( 'download_url' ) ) {
+    require_once( ABSPATH . 'wp-admin/includes/file.php' );
+}
+
+class shortpixel_api {
+
+    private $_apiKey = '';
+    private $_compressionType = '';
+    private $_maxAttempts = 10;
+
+    public function setCompressionType($compressionType)
+    {
+        if($compressionType == 'lossy') { $this->_compressionType = 1; }
+        else { $this->_compressionType = 0; }
+    }
+
+    public function getCompressionType()
+    {
+        return $this->_compressionType;
+    }
+
+    public function setApiKey($apiKey)
+    {
+        $this->_apiKey = $apiKey;
+    }
+
+    public function getApiKey()
+    {
+        return $this->_apiKey;
+    }
+
+    public function __construct($apiKey, $compressionType) {
+        $this->_apiKey = $apiKey;
+        $this->setCompressionType($compressionType);
+
+        add_action('processImageAction', array(&$this, 'processImageAction'), 10, 4);
+    }
+
+    public function processImageAction($url, $filePath, $ID, $time) {
+        $this->processImage($url, $filePath, $ID, $time);
+    }
+
+    public function doRequests($url, $filePath, $ID = null, $time = 0) {
+        $requestURL = 'https://api.shortpixel.com/reducer.php?key='.$this->_apiKey.'&lossy='.$this->_compressionType.'&url=';
+
+        $requestURL = $requestURL . urlencode($url);
+
+        $args = array('timeout'=> SP_MAX_TIMEOUT);
+
+        $response = wp_remote_get($requestURL, $args);
+
+        if(is_object($response) && get_class($response) == 'WP_Error') {
+            return false;
+        }
+
+        return $response;
+    }
+
+    public function parseResponse($response) {
+        $data = $response['body'];
+        $data = str_replace('Warning: Division by zero in /usr/local/important/web/api.shortpixel.com/lib/functions.php on line 33', '', $data);
+        $data = $this->parseJSON($data);
+        return $data;
+    }
+
+    //handles the processing of the image using the ShortPixel API
+    public function processImage($url, $filePath, $ID = null, $time = 0) {
+
+        $response = $this->doRequests($url, $filePath, $ID, $time);
+
+        if(!$response) return $response;
+
+        if($response['response']['code'] != 200) {
+            printf('Web service did not respond. Please try again later.');
+            return false;
+            //error
+        }
+
+        $data = $this->parseResponse($response);
+
+        switch($data->Status->Code) {
+            case 1:
+                //handle image has been scheduled
+                sleep(1);
+                return $this->processImage($url, $filePath, $ID, $time);
+                break;
+            case 2:
+                //handle image has been processed
+                $this->handleSuccess($data, $url, $filePath, $ID);
+                break;
+            case -16:
+                return 'Quota exceeded</br>';
+            case -17:
+                return 'Wrong API Key</br>';
+            default:
+                //handle error
+                return 'An error occurred while processing this image. Please try uploading it again.</br>';
+        }
+
+        return $data;
+    }
+
+
+    public function handleSuccess($callData, $url, $filePath, $ID) {
+        $tempFile = download_url(str_replace('https://','http://',urldecode($callData->DownloadURL)));
+
+        if ( is_wp_error( $tempFile ) ) {
+            @unlink($tempFile);
+            return printf("Error downloading file (%s)", $tempFile->get_error_message());
+        }
+
+        if (!file_exists($tempFile)) {
+            return printf("Unable to locate downloaded file (%s)", $tempFile);
+        }
+
+        @unlink( $filePath );
+        $success = @rename( $tempFile, $filePath );
+
+        if (!$success) {
+            $copySuccess = copy($tempFile, $filePath);
+            unlink($tempFile);
+        }
+
+        if($success || $copySuccess) {
+            //update statistics
+            if(isset($callData->LossySize)) {
+                $savedSpace = $callData->OriginalSize - $callData->LossySize;
+            } else {
+                $savedSpace = $callData->OriginalSize - $callData->SPSize;
+            }
+
+            update_option(
+                'wp-short-pixel-savedSpace',
+                get_option('wp-short-pixel-savedSpace') + $savedSpace
+            );
+            $averageCompression = get_option('wp-short-pixel-averageCompression') * get_option('wp-short-pixel-fileCount');
+            $averageCompression += $callData->PercentImprovement;
+            $averageCompression = $averageCompression /  (get_option('wp-short-pixel-fileCount') + 1);
+            update_option('wp-short-pixel-averageCompression', $averageCompression);
+            update_option('wp-short-pixel-fileCount', get_option('wp-short-pixel-fileCount')+1);
+
+            //update metadata
+            if(isset($ID)) {
+                $meta = wp_get_attachment_metadata($ID);
+                $meta['ShortPixelImprovement'] = $callData->PercentImprovement;
+                wp_update_attachment_metadata($ID, $meta);
+            }
+        }
+    }
+
+    public function parseJSON($data) {
+        if ( function_exists('json_decode') ) {
+            $data = json_decode( $data );
+        } else {
+            require_once( 'JSON/JSON.php' );
+            $json = new Services_JSON( );
+            $data = $json->decode( $data );
+        }
+        return $data;
+    }
+}
