92fa2d6ece27509faf60ff2dda2b61b1e6e9e821
ShortPixel <ShortPixel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-28 07:13:21 +0000

    * Corrected a bug affecting option saving for some of the users.
    git-svn-id: https://plugins.svn.wordpress.org/shortpixel-image-optimiser/trunk@1077243 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-shortpixel.php
+++ wp-shortpixel.php
@@ -3,7 +3,7 @@
  * Plugin Name: ShortPixel Image Optimiser
  * Plugin URI: https://shortpixel.com/
  * Description: ShortPixel is an image compression tool that helps improve your website performance. The plugin optimises images automatically using both lossy and lossless compression. Resulting, smaller, images are no different in quality from the original. To install: 1) Click the "Activate" link to the left of this description. 2) <a href="https://shortpixel.com/wp-apikey" target="_blank">Free Sign up</a> for your unique API Key . 3) Check your email for your API key. 4) Use your API key to activate ShortPixel plugin in the 'Plugins' menu in WordPress. 5) Done!
- * Version: 1.6.9
+ * Version: 1.6.10
  * Author: ShortPixel
  * Author URI: https://shortpixel.com
  */
@@ -62,29 +62,29 @@ class WPShortPixel {
 
     public function populateOptions() {
 
-        if(get_option('wp-short-pixel-apiKey') != false) {
+        if(get_option('wp-short-pixel-apiKey') !== false) {
             $this->_apiKey = get_option('wp-short-pixel-apiKey');
         } else {
             add_option( 'wp-short-pixel-apiKey', '', '', 'yes' );
         }
 
-        if(get_option('wp-short-pixel-verifiedKey') != false) {
+        if(get_option('wp-short-pixel-verifiedKey') !== false) {
             $this->_verifiedKey = get_option('wp-short-pixel-verifiedKey');
         }
 
-        if(get_option('wp-short-pixel-compression') != false) {
+        if(get_option('wp-short-pixel-compression') !== false) {
             $this->_compressionType = get_option('wp-short-pixel-compression');
         } else {
             add_option('wp-short-pixel-compression', $this->_compressionType, '', 'yes');
         }
 
-        if(get_option('wp-short-process_thumbnails') != false) {
+        if(get_option('wp-short-process_thumbnails') !== false) {
             $this->_processThumbnails = get_option('wp-short-process_thumbnails');
         } else {
             add_option('wp-short-process_thumbnails', $this->_processThumbnails, '', 'yes' );
         }
 
-        if(get_option('wp-short-backup_images') != false) {
+        if(get_option('wp-short-backup_images') !== false) {
             $this->_backupImages = get_option('wp-short-backup_images');
         } else {
             add_option('wp-short-backup_images', $this->_backupImages, '', 'yes' );
