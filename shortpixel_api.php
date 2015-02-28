ef87d3717f93baf3f75a5f18d9427744baa4bf5e
ShortPixel <ShortPixel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-28 01:14:54 +0000

    * small improvement to make the optimization of newly added images faster. * fixed condition when the PDF files weren't processed * different improvements
    git-svn-id: https://plugins.svn.wordpress.org/shortpixel-image-optimiser/tags/2.0.5@1101606 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- shortpixel_api.php
+++ shortpixel_api.php
@@ -1,162 +1,297 @@
 <?php
 if ( !function_exists( 'download_url' ) ) {
-    require_once( ABSPATH . 'wp-admin/includes/file.php' );
+	require_once( ABSPATH . 'wp-admin/includes/file.php' );
 }
 
 class shortpixel_api {
 
-    private $_apiKey = '';
-    private $_compressionType = '';
-    private $_maxAttempts = 10;
+	private $_apiKey = '';
+	private $_compressionType = '';
+	private $_maxAttempts = 10;
+	private $_apiEndPoint = 'https://api.shortpixel.com/v2/reducer.php';
 
-    public function setCompressionType($compressionType)
-    {
-        if($compressionType == 'lossy') { $this->_compressionType = 1; }
-        else { $this->_compressionType = 0; }
-    }
+	public function setCompressionType($compressionType) {
+		$this->_compressionType = $compressionType;
+	}
 
-    public function getCompressionType()
-    {
-        return $this->_compressionType;
-    }
+	public function getCompressionType() {
+		return $this->_compressionType;
+	}
 
-    public function setApiKey($apiKey)
-    {
-        $this->_apiKey = $apiKey;
-    }
+	public function setApiKey($apiKey) {
+		$this->_apiKey = $apiKey;
+	}
 
-    public function getApiKey()
-    {
-        return $this->_apiKey;
-    }
+	public function getApiKey() {
+		return $this->_apiKey;
+	}
 
-    public function __construct($apiKey, $compressionType) {
-        $this->_apiKey = $apiKey;
-        $this->setCompressionType($compressionType);
+	public function __construct($apiKey, $compressionType) {
+		$this->_apiKey = $apiKey;
+		$this->setCompressionType($compressionType);
 
-        add_action('processImageAction', array(&$this, 'processImageAction'), 10, 4);
-    }
+		add_action('processImageAction', array(&$this, 'processImageAction'), 10, 4);
+	}
 
-    public function processImageAction($url, $filePath, $ID, $time) {
-        $this->processImage($url, $filePath, $ID, $time);
-    }
+	public function processImageAction($url, $filePath, $ID, $time) {
+		$this->processImage($url, $filePath, $ID, $time);
+	}
 
-    public function doRequests($url, $filePath, $ID = null, $time = 0) {
-        $requestURL = 'https://api.shortpixel.com/reducer.php?key='.$this->_apiKey.'&lossy='.$this->_compressionType.'&url=';
+	public function doRequests($urls, $filePath, $ID = null) {
+		
+		if ( !is_array($urls) )
+			$response = $this->doBulkRequest(array($urls), true);
+		else
+			$response = $this->doBulkRequest($urls, true);
 
-        $requestURL = $requestURL . urlencode($url);
+		if(is_object($response) && get_class($response) == 'WP_Error') {
+			return false;
+		}
 
-        $args = array('timeout'=> SP_MAX_TIMEOUT);
+		return $response;
+	}
 
-        $response = wp_remote_get($requestURL, $args);
+	public function doBulkRequest($imageList = array(), $blocking = false) {
+		if(!is_array($imageList)) return false;
 
-        if(is_object($response) && get_class($response) == 'WP_Error') {
-            return false;
-        }
+		$requestParameters = array(
+			'key' => $this->_apiKey,
+			'lossy' => $this->_compressionType,
+			'urllist' => $imageList
+		);
+		
+		$response = wp_remote_post($this->_apiEndPoint, array(
+			'method' => 'POST',
+			'timeout' => 45,
+			'redirection' => 5,
+			'httpversion' => '1.0',
+			'blocking' => $blocking,
+			'headers' => array(),
+			'body' => json_encode($requestParameters),
+			'cookies' => array()
+		));
 
-        return $response;
-    }
+		return $response;
+	}
 
-    public function parseResponse($response) {
-        $data = $response['body'];
-        $data = str_replace('Warning: Division by zero in /usr/local/important/web/api.shortpixel.com/lib/functions.php on line 33', '', $data);
-        $data = $this->parseJSON($data);
-        return $data;
-    }
+	public function parseResponse($response) {
+		$data = $response['body'];
+		$data = str_replace('Warning: Division by zero in /usr/local/important/web/api.shortpixel.com/lib/functions.php on line 33', '', $data);
+		$data = $this->parseJSON($data);
+		return $data;
+	}
 
-    //handles the processing of the image using the ShortPixel API
-    public function processImage($url, $filePath, $ID = null, $time = 0) {
+	//handles the processing of the image using the ShortPixel API
+	public function processImage($url, $filePath, $ID = null, $startTime = 0) {
+		
+		if($startTime == 0) { $startTime = time(); }		
+		if(time() - $startTime > MAX_EXECUTION_TIME) {//keeps track of time
+			$meta = wp_get_attachment_metadata($ID);
+			$meta['ShortPixelImprovement'] = 'Could not determine compression';
+			unset($meta['ShortPixel']['WaitingProcessing']);
+			wp_update_attachment_metadata($ID, $meta);
+			return 'Could not determine compression';
+		}
 
-        $response = $this->doRequests($url, $filePath, $ID, $time);
+		$response = $this->doRequests($url, $filePath, $ID);//send requests to API
+		if(!$response) return $response;
 
-        if(!$response) return $response;
+		if($response['response']['code'] != 200) {//response <> 200 -> there was an error apparently?
+			printf('ShortPixel API service accesibility error. Please try again later.');
+			return false;
+		}
 
-        if($response['response']['code'] != 200) {
-            printf('Web service did not respond. Please try again later.');
-            return false;
-            //error
-        }
+		$data = $this->parseResponse($response);//get the actual  response from API
+		
+		if( !is_array($data) ) {// the answer from API isn't good
+			printf('Web service returned an error. Please try again later.');
+			return false;
+		}
+		
+		$firstImage = $data[0];//extract as object first image
+		
+		switch($firstImage->Status->Code) {
+			case 1:
+				//handle image has been scheduled
+				sleep(1);
+				return $this->processImage($url, $filePath, $ID, $startTime);
+				break;
+			case 2:
+				//handle image has been processed
+				$this->handleSuccess($data, $url, $filePath, $ID);
+				break;
+			case -403:
+				return 'Quota exceeded</br>';
+			case -401:
+				return 'Wrong API Key</br>';
+			case -302:
+				return 'Images does not exists</br>';
+			default:
+				//handle error
+				return $data->Status->Message;
+		}
 
-        $data = $this->parseResponse($response);
-
-        switch($data->Status->Code) {
-            case 1:
-                //handle image has been scheduled
-                sleep(1);
-                return $this->processImage($url, $filePath, $ID, $time);
-                break;
-            case 2:
-                //handle image has been processed
-                $this->handleSuccess($data, $url, $filePath, $ID);
-                break;
-            case -16:
-                return 'Quota exceeded</br>';
-            case -17:
-                return 'Wrong API Key</br>';
-            default:
-                //handle error
-                return 'An error occurred while processing this image. Please try uploading it again.</br>';
-        }
-
-        return $data;
-    }
+		return $data;
+	}
 
 
-    public function handleSuccess($callData, $url, $filePath, $ID) {
-        $tempFile = download_url(str_replace('https://','http://',urldecode($callData->DownloadURL)));
+	public function handleSuccess($callData, $url, $filePath, $ID) {
 
-        if ( is_wp_error( $tempFile ) ) {
-            @unlink($tempFile);
-            return printf("Error downloading file (%s)", $tempFile->get_error_message());
-        }
+		$counter = 0;
+		if($this->_compressionType)
+			{
+				$fileType = "LossyURL";
+				$fileSize = "LossySize";
+			}	
+		else
+			{
+				$fileType = "LosslessURL";
+				$fileSize = "LoselessSize";
+			}
 
-        if (!file_exists($tempFile)) {
-            return printf("Unable to locate downloaded file (%s)", $tempFile);
-        }
+		foreach ( $callData as $fileData )//download each file from array and process it
+		{
+			
+			if ( $counter == 0 )//save percent improvement for main file
+				$percentImprovement = $fileData->PercentImprovement;
 
-        @unlink( $filePath );
-        $success = @rename( $tempFile, $filePath );
+			$correctFileSize = $fileData->$fileSize;
+			$tempFiles[$counter] = download_url(urldecode($fileData->$fileType));
+			
+			if(is_wp_error( $tempFiles[$counter] )) //also tries with http instead of https
+				$tempFiles[$counter] = download_url(str_replace('https://', 'http://', urldecode($fileData->$fileType)));
+				
+			if ( is_wp_error( $tempFiles[$counter] ) ) {
+				@unlink($tempFiles[$counter]);
+				return sprintf("Error downloading file (%s)", $tempFiles[$counter]->get_error_message());
+				die;
+			}
+	
+			//check response so that download is OK
+			if( filesize($tempFiles[$counter]) != $correctFileSize) {
+				return sprintf("Error downloading file - incorrect file size");
+				die;
+			}
+	
+			if (!file_exists($tempFiles[$counter])) {
+				return sprintf("Unable to locate downloaded file (%s)", $tempFiles[$counter]);
+				die;
+			}	
+			
+			$counter++;
+		}
 
-        if (!$success) {
-            $copySuccess = copy($tempFile, $filePath);
-            unlink($tempFile);
-        }
+		//if backup is enabled
+		if(get_option('wp-short-backup_images')) 
+		{
 
-        if($success || $copySuccess) {
-            //update statistics
-            if(isset($callData->LossySize)) {
-                $savedSpace = $callData->OriginalSize - $callData->LossySize;
-            } else {
-                $savedSpace = $callData->OriginalSize - $callData->SPSize;
-            }
+			$imageIndex = 0;
+			$uploadDir = wp_upload_dir();
 
-            update_option(
-                'wp-short-pixel-savedSpace',
-                get_option('wp-short-pixel-savedSpace') + $savedSpace
-            );
-            $averageCompression = get_option('wp-short-pixel-averageCompression') * get_option('wp-short-pixel-fileCount');
-            $averageCompression += $callData->PercentImprovement;
-            $averageCompression = $averageCompression /  (get_option('wp-short-pixel-fileCount') + 1);
-            update_option('wp-short-pixel-averageCompression', $averageCompression);
-            update_option('wp-short-pixel-fileCount', get_option('wp-short-pixel-fileCount')+1);
+			if(!file_exists(SP_BACKUP_FOLDER) && !mkdir(SP_BACKUP_FOLDER, 0777, true)) {
+				return sprintf("Backup folder does not exist and it could not be created");
+			}
+			$meta = wp_get_attachment_metadata($ID);
+			$source[$imageIndex] = $filePath;
+			
+			//create destination dir if it isn't already created
+			@mkdir( SP_BACKUP_FOLDER . $uploadDir['subdir'], 0777, true);
+			
+			$destination[$imageIndex] = SP_BACKUP_FOLDER . $uploadDir['subdir'] . DIRECTORY_SEPARATOR . basename($source[$imageIndex]);
 
-            //update metadata
-            if(isset($ID)) {
-                $meta = wp_get_attachment_metadata($ID);
-                $meta['ShortPixelImprovement'] = $callData->PercentImprovement;
-                wp_update_attachment_metadata($ID, $meta);
-            }
-        }
-    }
+			foreach ( $meta['sizes'] as $pictureDetails )
+			{
+				$imageIndex++;
+				$source[$imageIndex] = $uploadDir['path'] . DIRECTORY_SEPARATOR . $pictureDetails['file'];
+				$destination[$imageIndex] = SP_BACKUP_FOLDER . $uploadDir['subdir'] . DIRECTORY_SEPARATOR . basename($source[$imageIndex]);
 
-    public function parseJSON($data) {
-        if ( function_exists('json_decode') ) {
-            $data = json_decode( $data );
-        } else {
-            require_once( 'JSON/JSON.php' );
-            $json = new Services_JSON( );
-            $data = $json->decode( $data );
-        }
-        return $data;
-    }
+			}
+
+
+			if(is_writable(SP_BACKUP_FOLDER)) {
+				if(!file_exists($destination[0])) 
+				{
+					foreach ( $source as $imageIndex => $fileSource )
+					{
+						$fileDestination = $destination[$imageIndex];
+						@copy($fileSource, $fileDestination);
+					}			
+					
+				}
+			} else {
+				return sprintf("Backup folder exists but is not writable");
+			}
+		}//end backup section
+
+
+		$counter = 0;
+		$meta = wp_get_attachment_metadata($ID);//we'll need the metadata for subdir
+		$SubDir = trim(substr($meta['file'],0,strrpos($meta['file'],"/")+1));
+		if ( strlen($SubDir) == 0 )//it is likely a PDF file so we treat this differently
+		{
+			global  $wpdb;
+			$qry = "SELECT * FROM " . $wpdb->prefix . "postmeta
+                WHERE  (
+					post_id = $ID AND
+					meta_key = '_wp_attached_file'
+					)";
+			$idList = $wpdb->get_results($qry);
+			$metaPDF = $idList[0];	
+			$SubDir = trim(substr($metaPDF->meta_value,0,strrpos($metaPDF->meta_value,"/")+1));
+		}
+		
+		foreach ( $tempFiles as $tempFile )
+		{ 
+			
+			$sourceFile = $tempFile;
+			$destinationFile = WP_CONTENT_DIR . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . $SubDir . basename($url[$counter]);	
+			
+			@unlink( $destinationFile );			
+			$success = @rename( $sourceFile, $destinationFile );
+	
+			if (!$success) {
+				$copySuccess = copy($sourceFile, $destinationFile);
+				unlink($sourceFile);
+			}
+	
+			//save data to counters
+			if($success || $copySuccess) {
+				//update statistics
+				$fileData = $callData[$counter];
+				$savedSpace = $fileData->OriginalSize - $fileData->LossySize;
+
+				update_option(
+					'wp-short-pixel-savedSpace',
+					get_option('wp-short-pixel-savedSpace') + $savedSpace
+				);
+				$averageCompression = get_option('wp-short-pixel-averageCompression') * get_option('wp-short-pixel-fileCount');
+				$averageCompression += $fileData->PercentImprovement;
+				$averageCompression = $averageCompression /  (get_option('wp-short-pixel-fileCount') + 1);
+				update_option('wp-short-pixel-averageCompression', $averageCompression);
+				update_option('wp-short-pixel-fileCount', get_option('wp-short-pixel-fileCount')+1);
+	
+			}
+			$counter++;
+		}	
+
+		//update metadata
+		if(isset($ID)) {
+			$meta = wp_get_attachment_metadata($ID);
+			$meta['ShortPixelImprovement'] = $percentImprovement;
+			wp_update_attachment_metadata($ID, $meta);
+		}
+		
+
+	}
+
+	public function parseJSON($data) {
+		if ( function_exists('json_decode') ) {
+			$data = json_decode( $data );
+		} else {
+			require_once( 'JSON/JSON.php' );
+			$json = new Services_JSON( );
+			$data = $json->decode( $data );
+		}
+		return $data;
+	}
 }
